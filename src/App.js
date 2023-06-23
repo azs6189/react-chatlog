c64294c21339c97bb350548a8876ef37aa82116c
Allie Soliz <solizale@gmail.com>
2023-06-23 11:59:59 -0400

    passes in props: sender, body, timeStamp into instance of ChatEntry

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
-
+import TimeStamp from './components/TimeStamp'
 // {
 //     "id": 1,
 //     "sender":"Vladimir",
@@ -19,7 +19,11 @@ const App = () => {
 			</header>
 			<main>
 				{/* Wave 01: Render one ChatEntry component */}
-				<ChatEntry></ChatEntry>
+				<ChatEntry
+					sender='Vladimir'
+					body='Why are you arguing with me'
+					timeStamp='2018-05-29T22:49:06+00:00'
+				></ChatEntry>
 
 				{/* Wave 02: Render ChatLog component */}
 			</main>
