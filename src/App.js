d9626cdd9e1fad8b5bbf4299d34bc5d0c1fefbd9
Allie Soliz <solizale@gmail.com>
2023-06-24 00:52:06 -0400

    cleans up App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
-
-// {
-//     "id": 1,
-//     "sender":"Vladimir",
-//     "body":"why are you arguing with me",
-//     "timeStamp":"2018-05-29T22:49:06+00:00",
-//     "liked": false
-//   },
 
 const App = () => {
 	return (
